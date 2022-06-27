a3b914a2f097449e5d47c3bd3151141f8c8598db
G3 <aljawharayr@gmail.com>
2022-06-27 12:03:18 +0300

    //

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { AbstractControl ,FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    number: new FormControl(0),
+    reason: new FormControl(''),
+    fnumber: new FormControl(0),
+
+  });
+  submitted = false;
+
+  constructor(private formBulider: FormBuilder){}
+  ngOnInit(): void{
+    this.form = this.formBulider.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]],
+      phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      reason: ['', Validators.required],
+      fnumber: ['', Validators.required]
+    })
+  }
+  get f(): { [key: string]: AbstractControl} {
+    return this.form.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid){
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2))
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDnQd23jLcctfOna5soM9z2lwYkq4yloRg",
+    authDomain: "todolistapp-308e9.firebaseapp.com",
+    projectId: "todolistapp-308e9",
+    storageBucket: "todolistapp-308e9.appspot.com",
+    messagingSenderId: "157898295937",
+    appId: "1:157898295937:web:7d61b41bb9a90815d6db80",
+    measurementId: "G-BXR7TNH2TV"
+  }
 };
 
 /*
